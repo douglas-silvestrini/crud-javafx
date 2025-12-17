0746aba8e738a1ac626cec9caca3ef2207c9d9ce
Douglas Silvestrini <douglassilvestrini@gmail.com>
2025-12-17 09:29:59 -0300

    executavel .jar

--- src/main/java/br/com/douglasinformatica/App.java
+++ src/main/java/br/com/douglasinformatica/App.java
@@ -31,8 +31,10 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+    /*
+     * public static void main(String[] args) {
+     * launch();
+     * }
+     */
 
 }
---
+++ src/main/java/br/com/douglasinformatica/Main.java
@@ -0,0 +1,14 @@
+package br.com.douglasinformatica;
+
+import javafx.application.Application;
+
+/**
+ * JavaFX App
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(App.class);
+    }
+
+}
